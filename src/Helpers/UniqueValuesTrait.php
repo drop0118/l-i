7f73c00536271a796a1b2d29ffadc21e72731989
RoyWang <yuelongnet@163.com>
2020-04-28 22:16:44 +0800

    Update UniqueValuesTrait.php

--- src/Helpers/UniqueValuesTrait.php
+++ src/Helpers/UniqueValuesTrait.php
@@ -78,10 +78,10 @@ trait UniqueValuesTrait
         mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $hyphen = chr(45);// "-"
-        $uuid = substr($charid, 0, 8).$hyphen
-                .substr($charid, 8, 4).$hyphen
-                .substr($charid, 12, 4).$hyphen
-                .substr($charid, 16, 4).$hyphen
+        $uuid = substr($charid, 0, 8)
+                .substr($charid, 8, 4)
+                .substr($charid, 12, 4)
+                .substr($charid, 16, 4)
                 .substr($charid, 20, 12);
         return $uuid;
     }
